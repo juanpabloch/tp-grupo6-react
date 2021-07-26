7e85a13cb281472ea7dd6ef464d691dac752507d
juanpabloch <63684218+juanpabloch@users.noreply.github.com>
2021-07-26 12:48:12 -0300

    estilos

--- src/components/Personas/ListPersona.js
+++ src/components/Personas/ListPersona.js
@@ -7,12 +7,12 @@ export default function ListPersonas({ personas }) {
         <li key={persona.persona_id} className="list-group-item d-flex justify-content-between align-items-center">
           {persona.nombre}
           <div className="btn btn-group">
-              <Link className="btn btn-danger" to={`/personas/delete/${persona.persona_id}`}>
+              <Link className="btn btn-outline-danger" to={`/personas/delete/${persona.persona_id}`}>
                 Borrar
               </Link>
 
               <Link
-                className="btn btn-warning"
+                className="btn btn-outline-dark"
                 to={{
                   pathname: `/personas/modificar/${persona.persona_id}/${persona.nombre}/${persona.apellido}/${persona.alias}/${persona.email}`,
                 }}>
