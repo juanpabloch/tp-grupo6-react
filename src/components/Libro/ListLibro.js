580f6dcb6f6fc57379173c0addf7ca105538e027
juanpabloch <63684218+juanpabloch@users.noreply.github.com>
2021-07-27 01:13:55 -0300

    estilos 2

--- src/components/Libro/ListLibro.js
+++ src/components/Libro/ListLibro.js
@@ -6,7 +6,7 @@ export default function ListLibro({ libros }) {
   return (
     <div className="row">
       {libros.map((libro) => (
-        <li key={libro.libro_id} className="list-group-item d-flex justify-content-between">
+        <li key={libro.libro_id} className="list-group-item d-flex justify-content-between 4">
           <p>{libro.nombre} <span>{libro.persona_id?<span className="badge bg-warning">PRESTADO</span>:null}</span> </p>
           <div className="btn-group" role="group" aria-label="Basic example">
                 <Link className="btn btn-outline-info mr-10" to={`/libro/${libro.libro_id}/detalle`}>
