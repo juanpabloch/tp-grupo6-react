7f756c6b2e8d11259838fa4d874871189aa4c163
agustinmme <76716674+agustinmme@users.noreply.github.com>
2021-07-28 14:12:33 -0300

    Modificacion alertaApp  y 404

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { BrowserRouter as Router,Route,Switch } from "react-router-dom"; 
-import axios from 'axios';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axios from "axios";
 import "./App.css";
 
 import NavBar from "./Navbar/Navbar";
 import Borrar from "./Borrar/Borrar";
 //import libro
 import Libros from "./Libro/Libros";
-import FormularioNuevoLibro from './Libro/FormularioNuevoLibro'
+import FormularioNuevoLibro from "./Libro/FormularioNuevoLibro";
 import ModificarLibro from "./Libro/Modificar";
 import DetalleLibro from "./Libro/Detalles";
-import FormPrestarLibro from './Libro/FormPrestarLibro';
-import Devolver from './Libro/Devolver';
+import FormPrestarLibro from "./Libro/FormPrestarLibro";
+import Devolver from "./Libro/Devolver";
 //import personas
 import Personas from "./Personas/Personas";
 import ModificarPersona from "./Personas/Modificar/Modificar";
@@ -20,56 +20,67 @@ import FormularioNuevaPersona from "./Personas/Agregar";
 import DetallePersona from "./Personas/Detalles";
 //import categorias
 import Categorias from "./Categorias/Categorias";
-import FormularioNuevaCategoria from './Categorias/FormularioNuevaCategoria'
-import ModificarCategoria from './Categorias/Modificar'
+import FormularioNuevaCategoria from "./Categorias/FormularioNuevaCategoria";
+import ModificarCategoria from "./Categorias/Modificar";
 
-import NotFound from './NotFound/NotFound'
+import NotFound from "./NotFound/NotFound";
 const App = () => {
-  
   const dispatch = useDispatch();
 
-  const [alerta, setAlerta] = useState({mostrar:false,msg:""});
+  const [alerta, setAlerta] = useState({ mostrar: false, msg: "" });
 
   useEffect(() => {
-
     const fetchAll = async () => {
       try {
-      let respuesta = await axios.get("https://tp-grupo6-api.herokuapp.com/libro");
-      dispatch({ type: "AGREGAR_LISTADO_LIBROS", listado: respuesta.data });
-       respuesta = await axios.get(
-        "https://tp-grupo6-api.herokuapp.com/categoria"
-      );
-      dispatch({ type: "AGREGAR_LISTADO_CATEGORIA", listado: respuesta.data });
-       respuesta = await axios.get("https://tp-grupo6-api.herokuapp.com/persona");
-      dispatch({ type: "AGREGAR_LISTADO_PERSONA", listado: respuesta.data });
-    } catch (error) {
-      const newState = JSON.parse(JSON.stringify(alerta));
-      newState.mostrar = true;
-      newState.msg = error.response.data.mensaje;
-      setAlerta(newState);
-    }
+        let respuesta = await axios.get(
+          "https://tp-grupo6-api.herokuapp.com/libro"
+        );
+        dispatch({ type: "AGREGAR_LISTADO_LIBROS", listado: respuesta.data });
+        respuesta = await axios.get(
+          "https://tp-grupo6-api.herokuapp.com/categoria"
+        );
+        dispatch({
+          type: "AGREGAR_LISTADO_CATEGORIA",
+          listado: respuesta.data,
+        });
+        respuesta = await axios.get(
+          "https://tp-grupo6-api.herokuapp.com/persona"
+        );
+        dispatch({ type: "AGREGAR_LISTADO_PERSONA", listado: respuesta.data });
+      } catch (error) {
+        const newState = JSON.parse(JSON.stringify(alerta));
+        newState.mostrar = true;
+        newState.msg = error.response.data.mensaje;
+        setAlerta(newState);
+      }
     };
     fetchAll();
   }, []);
 
-  const handleCerrar = (e) => {
-    const newForm = JSON.parse(JSON.stringify(alerta));
-    newForm.mostrar = false;
-    setAlerta(newForm);
-  };
- 
   return (
-
     <div className="bg">
       <div className="container">
-      {  alerta.mostrar?<div className="alert alert-danger alert-dismissible fade show" role="alert">
-              <strong>Error! </strong>{alerta.msg}
-              <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={handleCerrar}></button>
-                </div>:
-        <div className="row py-3 px-2 flex-container">
-          <div className="col-md-10 mx-auto">
-            <div className="bg-white shadow rounded overflow-hidden Fondo">
-              {/* <div className="px-4 pt-0 pb-4 cover header_principal">
+        {alerta.mostrar ? (
+          <div class="row h-100">
+          <div class="col-sm-12 my-auto ">
+            <div className="d-flex justify-content-center">
+          <div class="alert alert-danger p-3 " role="alert">
+              <h2 class="alert-heading">
+                <strong>Error! </strong>
+              </h2>
+              <p>{alerta.msg}</p>
+              <hr/>
+              <p class="mb-0">Por favor Recargue la pagina.</p>
+              </div>
+            </div>
+          </div>
+       </div>
+
+        ) : (
+          <div className="row py-3 px-2 flex-container">
+            <div className="col-md-10 mx-auto">
+              <div className="bg-white shadow rounded overflow-hidden Fondo">
+                {/* <div className="px-4 pt-0 pb-4 cover header_principal">
                 <div className="media align-items-end profile-head">
                   <div className="media-body mb-2 text-white">
                     <h4 className="mt-0 mb-0">Grupo 6</h4>
@@ -81,41 +92,86 @@ const App = () => {
                   </div>
                 </div>
               </div> */}
-                
-                  
-              <Router >
-                <div className="bg-light p-3 text-center navegador">
-                  <NavBar/>
-                </div>
-                <Switch>
-                <Route exact path="/" component={Libros} />
-                <Route exact path="/modificar-libro/:id/:nombre/:categoria/:persona/:descripcion"  component={ModificarLibro} />
-                <Route exact path="/delete/:id/:tipo" component={Borrar} />
-                <Route exact path="/libro/:id/detalle" component={DetalleLibro} />
-                <Route exact path="/libro/nuevo" component={FormularioNuevoLibro} />
-                <Route exact path="/libro/devolver/:id" component={Devolver} />
-                <Route exact path="/prestarlibro/:id/:nombre" component={FormPrestarLibro}/>
-                <Route exact path="/personas" component={Personas} />
-                <Route exact path="/personas/modificar/:id/:nombre/:apellido/:alias/:email" component={ModificarPersona} />
-                <Route exact path="/personas/delete/:id/:tipo" component={Borrar} />
-                <Route exact path="/personas/detalle/:id/:nombre/:apellido/:alias/:email" component={DetallePersona} />
-                <Route exact path="/personas/nuevo" component={FormularioNuevaPersona} />
 
-                <Route exact path="/categorias" component={Categorias} />
-                <Route exact path="/categoria/nuevo" component={FormularioNuevaCategoria} />
-                <Route exact path="/categoria/modificar-categoria/:id/:nombre" component={ModificarCategoria} />
-                <Route exact path="/categoria/delete/:id/:tipo" component={Borrar} />
-                <Route  component={NotFound} />
-                </Switch>
-             </Router>
-            
+                <Router>
+                  <div className="bg-light p-3 text-center navegador">
+                    <NavBar />
+                  </div>
+                  <Switch>
+                    <Route exact path="/" component={Libros} />
+                    <Route
+                      exact
+                      path="/modificar-libro/:id/:nombre/:categoria/:persona/:descripcion"
+                      component={ModificarLibro}
+                    />
+                    <Route exact path="/delete/:id/:tipo" component={Borrar} />
+                    <Route
+                      exact
+                      path="/libro/:id/detalle"
+                      component={DetalleLibro}
+                    />
+                    <Route
+                      exact
+                      path="/libro/nuevo"
+                      component={FormularioNuevoLibro}
+                    />
+                    <Route
+                      exact
+                      path="/libro/devolver/:id"
+                      component={Devolver}
+                    />
+                    <Route
+                      exact
+                      path="/prestarlibro/:id/:nombre"
+                      component={FormPrestarLibro}
+                    />
+                    <Route exact path="/personas" component={Personas} />
+                    <Route
+                      exact
+                      path="/personas/modificar/:id/:nombre/:apellido/:alias/:email"
+                      component={ModificarPersona}
+                    />
+                    <Route
+                      exact
+                      path="/personas/delete/:id/:tipo"
+                      component={Borrar}
+                    />
+                    <Route
+                      exact
+                      path="/personas/detalle/:id/:nombre/:apellido/:alias/:email"
+                      component={DetallePersona}
+                    />
+                    <Route
+                      exact
+                      path="/personas/nuevo"
+                      component={FormularioNuevaPersona}
+                    />
+
+                    <Route exact path="/categorias" component={Categorias} />
+                    <Route
+                      exact
+                      path="/categoria/nuevo"
+                      component={FormularioNuevaCategoria}
+                    />
+                    <Route
+                      exact
+                      path="/categoria/modificar-categoria/:id/:nombre"
+                      component={ModificarCategoria}
+                    />
+                    <Route
+                      exact
+                      path="/categoria/delete/:id/:tipo"
+                      component={Borrar}
+                    />
+                    <Route component={NotFound} />
+                  </Switch>
+                </Router>
+              </div>
             </div>
           </div>
-        </div>
-                }
+        )}
       </div>
     </div>
-
   );
 };
 
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import './NotFound.css'
+import {Link} from 'react-router-dom'
 export default function NotFound() {
     return (
-        <div className="container p-5 d-flex justify-content-center">
-                  <img src="./img/notfound.svg" alt="notfound,notfoundsvg"/>
-      </div>
-      
+<div className="container">
+<h1 className='titulo'>404</h1>
+<p className='texto'>No pudimos encontrar la página que buscaba. </p>
+<div align="center">
+  <Link className="btn btn-primary btn-agregar m-3" to={"/"}> Volver a Inicio</Link>
+</div>
+</div>
     )
 }
