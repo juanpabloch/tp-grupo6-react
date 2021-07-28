4a9bd4db8f9c271807f8a3f5162ec0507da4c114
agustinmme <76716674+agustinmme@users.noreply.github.com>
2021-07-28 14:46:38 -0300

    Fix persona by david

--- src/components/Personas/Detalles.js
+++ src/components/Personas/Detalles.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import { useParams, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 const Detalle = () => {
   const { id,nombre,alias,email,apellido } = useParams();
   const libros = useSelector((state) => state.libros.listado);
-
+  const [librosPretados,setLibrosPrestados] = useState();
   const librosArray = libros.filter(
     (libro) => libro.persona_id === parseInt(id)
   );
   const LibrosToMostar = librosArray.map(function (item) {
     return item.nombre;
   });
+  useEffect(() => {
+    setLibrosPrestados(LibrosToMostar.join());
+  }, []);
   return (
     <div className="container p-5">
       <div className="card">
@@ -18,8 +21,8 @@ const Detalle = () => {
           <span
             className="d-inline-block text-truncate"
           >
-            {LibrosToMostar.join()
-              ? `Libros prestados: ${LibrosToMostar.join()}`
+            {librosPretados
+              ? `Libros prestados: ${librosPretados}`
               : `Sin libros`}
           </span>
           <Link className="link-volver-libro" to="/personas">
@@ -32,7 +35,7 @@ const Detalle = () => {
           <p className="card-text">{alias}</p>
           <p className="card-text">{email}</p>
           <Link
-            className={!LibrosToMostar.join()?"btn btn-outline-danger":"btn btn-outline-danger disabled"}
+            className={!librosPretados?"btn btn-outline-danger":"btn btn-outline-danger disabled"}
             to={`/personas/delete/${id}/persona`}
             style={{marginRight: '10px'}}
             
